1f00ae63e517d211e94c4807a806f1a0b71285db
ameliacode <emilyjr1@naver.com>
2021-11-26 14:49:01 +0900

    Update Assignment-2

--- Assignment-2/2021122543_assignment_2.py
+++ Assignment-2/2021122543_assignment_2.py
@@ -1,14 +1,10 @@
 import os
-<<<<<<< HEAD
 import random
-=======
->>>>>>> 1ed69f9fff3b6bba766d2336390049b725912647
 
 class Environment:
     def __init__(self):
-        self.map = []
+        self.rewards = []
         self.QTable = [[0 for _ in range(4)] for _ in range(25)]
-<<<<<<< HEAD
         self.actions = {"up":0,"right":1,"left":2,"down":3}
         self.states = {"S":0, "G":100, "T":1, "B":-100, "P": 0}
         self.gamma = 0.9
@@ -47,37 +43,27 @@ class Environment:
         if not next_move:
             pass
         else:
-            self.QTable[state][_action] = self.map[state] + self.gamma * max(self.QTable[next_move])
-=======
-        self.actions = ["up","right","left","down"]
-        self.states = {"S":0, "G":100, "T":1, "B":-100, "P": 0}
-        self.gamma = 0.9
-        
-    def Q(self, state, action): #state: position index, action: self.actions
-        pass
->>>>>>> 1ed69f9fff3b6bba766d2336390049b725912647
+            self.QTable[state][_action] = self.rewards[state] + self.gamma * max(self.QTable[next_move])
 
     def import_env(self):
         # current directory
         currentDir = os.path.dirname(os.path.abspath(__file__))
         currentDir.replace("\\","\\\\")
         try:
-            with open(currentDir+"\\input.txt","r") as inputFile:
-<<<<<<< HEAD
+            #window
+            #with open(currentDir+"\\input.txt","r") as inputFile:
+
+            #ubuntu
+            with open(currentDir+"/input.txt","r") as inputFile:
                 num_line = 0
                 for line in inputFile:
                     for i in range(5):
-                        self.map.append(self.states[line[i]])
+                        self.rewards.append(self.states[line[i]])
                         if line[i]=="S":
                             self.start = num_line*5+i
                         if line[i]=="G":
                             self.goal = num_line*5+i
-=======
-                for line in inputFile:
-                    self.map.append([self.states[line[i]] for i in range(5)])
 
-                
->>>>>>> 1ed69f9fff3b6bba766d2336390049b725912647
         except Exception as error:
             print(error)
 
@@ -85,11 +71,9 @@ class Environment:
 def main():
 
     env = Environment()
-<<<<<<< HEAD
     env.import_env()
-    print(env.map)
 
-    for step in range(1):
+    for ith_episode in range(10):
         pos = env.start
         while True:
             _action = random.choice(list(env.actions.keys()))
@@ -97,14 +81,10 @@ def main():
             pos = env.current_state
             if pos == env.goal:
                 break
+
     for i in range(25):
-        print(env.QTable[i])
-=======
-    
-    env.import_env()
-    print(env.map)
->>>>>>> 1ed69f9fff3b6bba766d2336390049b725912647
-    
+        print(i, env.QTable[i])
+
 
 if __name__=="__main__":
     main()
